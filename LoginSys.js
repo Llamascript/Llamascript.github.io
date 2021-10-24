004704c5b51d6eae50790f22de5d1d42b939a11d
Llamascript <ezekielmtz@gmail.com>
2021-10-23 22:20:54 -0300

    lots of stuff, actually

--- LoginSys.js
+++ LoginSys.js
@@ -1,15 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // --------- Sign in and Sign up Forms  -------------- //
-    const LoginForm = document.querySelector("#Sign-in-Form")
-    const SignupForm = document.querySelector("#Sign-up-Form")
+const LoginForm = document.querySelector('#Sign-in-Form');
+const SignupForm = document.querySelector('#Sign-up-Form');
 
-    document.querySelector("#Link-Signup").addEventListener("click", () => {
-        LoginForm.classList.add("form-hidden");
-        SignupForm.classList.remove("form-hidden");
+document.addEventListener('DOMContentLoaded', () => {
+    // --------- Sign in and Sign up Forms  -------------- //
+    document.querySelector('#Link-Signup').addEventListener('click', () => {
+        LoginForm.classList.add('form-hidden');
+        SignupForm.classList.remove('form-hidden');
     });
 
-    document.querySelector("#Link-Login").addEventListener("click", () => {
-        LoginForm.classList.remove("form-hidden");
-        SignupForm.classList.add("form-hidden");
+    document.querySelector('#Link-Login').addEventListener('click', () => {
+        LoginForm.classList.remove('form-hidden');
+        SignupForm.classList.add('form-hidden');
     });
 });
+ // ------------- FORM VALIDATION ----------- //
+ window.onload = start;
+
+ const suSubmit = document.querySelector('Submit') 
+ var fullname = document.querySelector('#Name');
+
+function start() {
+    SignupForm.addEventListener('submit', validate, false);
+}
+
+// -------------- NAME VALIDATION --------------- //
+function nameValidation() {
+    if (/^[a-zA-Z ]+$-?'?/.test(fullname.value)) {  //ojo con el - //
+        return true;
+    } else {
+    alert('Your name should only have valid characters.');
+    return false;
+    }
+ }
+
+ function validate(event) {
+     if (!nameValidation()) {
+         event.preventDefault();
+         event.stopPropagation();
+         return false;
+     }
+ }
